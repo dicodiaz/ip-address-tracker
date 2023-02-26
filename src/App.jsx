233250d8adf1dc00de2b482205f892f275396630
Dico Diaz <dicodiaz@gmail.com>
2023-02-25 22:35:43 -0500

    add react-icons, Rubik, react-bootstrap, axios, @testing-library/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { FaReact } from 'react-icons/fa';
+import { SiVite } from 'react-icons/si';
 import './App.css';
 
 function App() {
@@ -8,13 +11,15 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <SiVite />
         </a>
-        <a href="https://reactjs.org" target="_blank"></a>
+        <a href="https://reactjs.org" target="_blank">
+          <FaReact className="logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
